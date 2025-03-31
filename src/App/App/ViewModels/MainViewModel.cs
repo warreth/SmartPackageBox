bf622209ce65381518edd3e818c1e1eb56e4dc9b
WarreTh <info@wath.dev>
2025-03-31 19:06:50 +0200

    App: Implement Function to refresh photo (using OnPropertyChanged)

--- src/App/App/ViewModels/MainViewModel.cs
+++ src/App/App/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -6,11 +7,27 @@ namespace App.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+
     [ObservableProperty]
-    private string _imageUrl = "https://i.pinimg.com/736x/6a/05/12/6a0512fe6231884a99be9a7c31df2e2e.jpg";
+    private string _imageUrl = "https://picsum.photos/200/300";
+
+    [ObservableProperty]
+    private string _imageTimestamp = "";
+
+    public string FullImageUrl => $"{ImageUrl}?t={ImageTimestamp}";
 
     [RelayCommand]
-    private void OpenHatch() //TODO: Run this in a seperate thread
+    private void RefreshImage()
+    {
+        ImageTimestamp = DateTime.Now.Ticks.ToString();
+        OnPropertyChanged(nameof(FullImageUrl)); //Or OnPropertyChanged("FullImageUrl"); -> Let the UI know that the property has changed
+
+        //System.Console.WriteLine("Refreshed Image");
+        //System.Console.WriteLine($"Url is {ImageUrl}?t={ImageTimestamp}");
+    }
+
+    [RelayCommand]
+    private void OpenHatch() //TODO: Run this in a seperate thread or make async so the UI can't freeze
     {
         System.Console.WriteLine("Opened Hatch");
     }
